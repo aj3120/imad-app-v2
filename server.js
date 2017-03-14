eca164343abdfc4659063b8d55e9f1afa0838af5
aj3120 <aj3120@gmail.com>
2017-03-14 14:50:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/article-one',function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-two',function(req,res) {
-    res.send('Article three requested & will be serve')
+app.get('/main.js',function(req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/article-three',function(req,res) {
     res.send('Article three requested & will be serve')
